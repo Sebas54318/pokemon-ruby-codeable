8210535c97fbb67ab5ac82c1cb403d0e28300d9c
Sebastian Maguiña <54318sebas@gmail.com>
2023-02-25 17:15:35 -0500

    add fixed coding style

--- player.rb
+++ player.rb
@@ -2,52 +2,31 @@
 require_relative "pokemon"
 
 class Player
-
   def initialize(player_name, pokemon_specie, pokemon_name = nil, pokemon_level = nil)
     @name = player_name
     @pokemon = Pokemon.new(pokemon_specie, pokemon_name, pokemon_level)
   end
 
   def select_move
-    # Complete this
-    # EJEMPLO:
-    continue = true
-     while continue
+    loop do
       puts "#{@name}, select your move:"
       @pokemon.moves.each.with_index do |option, index|
-        print "#{index+1}. #{option}    "   
-      end  
-      
+        print "#{index + 1}. #{option}    "
+      end
       print "\n>"
-      # puts "1. #{@pokemon.moves[0]}      2. #{@pokemon.moves[1]}"
       input = gets.chomp
       if @pokemon.moves.include?(input)
         @pokemon.apply_current_move(input)
-        continue = false
-      else 
+        break
+      else
         puts "#{@pokemon.name} can't use this move"
       end
-
     end
-  
-    # Pokemon.new.apply_current_move  
-
-    # Great master, select your move:
-    # 1. scratch      2. ember
-    # >
-
-    # Una vez que obtenemos un moviemiento valido
-    # ejecutar al metodo apply_current_move de mi pokemon pasandole el movimiento
   end
 end
 
 # Create a class Bot that inherits from Player and override the select_move method
 
-player1 = Player.new("Pepe", "Charmander")
-player1.select_move
-# Pepe, select your move
-# 1. scratch      2. ember
-# > ember
- p player1
-# <PlayerX423423423, @name="Pepe"... 
-# @pokemon = <PokemonX34i34 ... @current_move={name: "ember", ...}  ...>>
+# player1 = Player.new("Pepe", "Charmander")
+# player1.select_move
+# p player1

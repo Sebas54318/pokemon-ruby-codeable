120651e03d0e8efbf520e6bd70db79f79bfd7e8c
Sebastian Maguiña <54318sebas@gmail.com>
2023-02-25 22:34:10 -0500

    Create classbot
    Co-authored-by: rorerorero <rorerorero@users.noreply.github.com>
    Co-authored-by: Diego Torres <diegotc86@gmail.com>
    Co-authored-by: Yahaira Collado <yahairacollado@users.noreply.github.com>

--- player.rb
+++ player.rb
@@ -1,5 +1,6 @@
 # require neccesary files
 require_relative "pokemon"
+require_relative "pokedex/pokemons"
 
 class Player
   def initialize(player_name, pokemon_specie, pokemon_name = nil, pokemon_level = nil)
@@ -25,8 +26,14 @@ class Player
   end
 end
 
-# Create a class Bot that inherits from Player and override the select_move method
+class Bot < Player
+  def select_move
+    move = @pokemon.moves.sample
+    @pokemon.apply_current_move(move)
+  end
+end
 
-# player1 = Player.new("Pepe", "Charmander")
-# player1.select_move
-# p player1
+# pokemon = Pokedex::POKEMONS.keys.sample
+# bot1 = Bot.new("Random Person", pokemon)
+# bot1.select_move
+# p bot1
